a46ecc021385f8399522fd226a5b43b9bd2a8d6b
Bauxi <valencia.diego825@gmail.com>
2023-04-30 11:50:08 +0200

    updated name

--- social_book/settings.py
+++ social_book/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y6on4&^y1+^c$5_h4eh-i2fz-c6jmagos9$5b!o!%8=08s#!tm'
+SECRET_KEY = 'wnuh$87dn8#d@pj+@nd23nn=+r#jn#95%nvu-%jrhxi%l$#p+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
